d2d1d51044346bdd1f8755fe40a425d8be05262b
Nate Derbinsky <nate.derbinsky@gmail.com>
2016-12-15 11:12:26 -0500

    import

---
+++ js/events.js
@@ -0,0 +1,200 @@
+var events = [
+
+	{
+		'date':'2007',
+		'title':'Intercultural Center',
+		'content':'Intercultural Center opens.',
+		'link':'https://www.wit.edu/student-life/student-services/international-student-services',
+		'type':'international',
+		'history':false,
+	},
+
+	{
+		'date':'2007',
+		'title':'Campus Climate Survey',
+		'content':'Campus Climate Survey begins. This biannual survey gauges Wentworth\'s openness and acceptance of all students. Results direct programs and approaches taken on campus.',
+		'link':'https://www.wit.edu/student-life/student-services/center-diversity-and-social-justice/programs-and-resources/campus-climate',
+		'type':'equal',
+		'history':false,
+	},
+
+	{
+		'date':'2007',
+		'title':'SLAM Launches',
+		'content':'SLAM is designed to increase the interest level in mathematics for high school students by exposing them to mathematics in fun and innovative ways.',
+		'link':'http://legacy.wit.edu/news/publications/sum2007/slam.pdf',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'2007',
+		'title':'SHPE @ WIT',
+		'content':'Founding of the Society of Hispanic Professional Engineers student chapter at Wentworth. This club is still active on campus.',
+		'link':'http://myweb.wit.edu/shpe/',
+		'type':'race',
+		'history':false,
+	},
+
+	{
+		'date':'2005',
+		'title':'Zorica Pantic: WIT President',
+		'content':'Dr. Zorica Pantic becomes Wentworth\'s first female president. She is also the first female president of any technical college in the United States.',
+		'link':'https://en.wikipedia.org/wiki/Zorica_Pantic',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'2005',
+		'title':'Women\'s Leadership Conference',
+		'content':'The annual Women\'s Leadership Conference starts.',
+		'link':'https://www.wit.edu/student-life/student-leadership',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'2002',
+		'title':'Multicultural Student Association @ WIT',
+		'content':'Founding of Multicultural Student Association at Wentworth. This club is still active on campus.',
+		'link':'https://www.facebook.com/witmsa',
+		'type':'international',
+		'history':false,
+	},
+
+	{
+		'date':'1995',
+		'title':'Accommodations for Students',
+		'content':'Formal accommodations for students with disabilities begins.',
+		'link':'https://www.wit.edu/student-life/student-services/counseling/disability-services',
+		'type':'disability',
+		'history':false,
+	},
+
+	{
+		'date':'1994',
+		'title':'Islamic Student Society @ WIT',
+		'content':'Islamic Student Society forms at Wentworth.',
+		'link':'https://www.facebook.com/IslamicSocietyofWIT',
+		'type':'religion',
+		'history':false,
+	},
+
+	{
+		'date':'1993',
+		'title':'NSBE @ WIT',
+		'content':'Founding of National Society of Black Engineers student chapter at Wentworth. This club is still active on campus.',
+		'link':'http://witnsbe.org',
+		'type':'race',
+		'history':false,
+	},
+
+	{
+		'date':'1990',
+		'title':'ADA',
+		'content':'American’s with Disabilities Act (ADA) passes into law.',
+		'link':'https://en.wikipedia.org/wiki/Americans_with_Disabilities_Act_of_1990',
+		'type':'disability',
+		'history':true,
+	},
+
+	{
+		'date':'1986',
+		'title':'SWE @ WIT',
+		'content':'Founding of Society of Women Engineers student chapter at Wentworth. This club is still active on campus.',
+		'link':'http://legacy.wit.edu/news/2013/03/2013-04-03.html',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'1985',
+		'title':'Disability Services',
+		'content':'Support services for students with disabilities begins.',
+		'link':'https://www.wit.edu/student-life/student-services/counseling/disability-services',
+		'type':'disability',
+		'history':false,
+	},
+
+	{
+		'date':'1985',
+		'title':'Varsity Sports for Women',
+		'content':'Basketball becomes the first varsity sport for women on campus.',
+		'link':'http://www.wentworthathletics.com/information/history/tradition',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'1981',
+		'title':'National Technical Association',
+		'content':'Founding of the National Technical Association student chapter at Wentworth. The NTA promotes minorities in technical fields.',
+		'link':null,
+		'type':'equal',
+		'history':false,
+	},
+
+	{
+		'date':'1980',
+		'title':'International Student Club',
+		'content':'Founding of International Student Club at Wentworth.',
+		'link':'https://www.wit.edu/student-life/student-organizations-activities#culture',
+		'type':'international',
+		'history':false,
+	},
+
+	{
+		'date':'1974',
+		'title':'First Female Graduate',
+		'content':'Susan Hardt, BCS becomes first woman to graduate from Wentworth.',
+		'link':null,
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'1972',
+		'title':'Female Students at WIT',
+		'content':'Wentworth admits women. Five female students enroll.',
+		'link':'http://legacy.wit.edu/news/publications/sum2007/women.pdf',
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'1972',
+		'title':'First Female Professor',
+		'content':'Lois Ascher becomes Wentworth’s first female professor.',
+		'link':null,
+		'type':'women',
+		'history':false,
+	},
+
+	{
+		'date':'1964',
+		'title':'WIT Faculty/Staff Non-Discrimination',
+		'content':'Wentworth adopts a non-discrimination policy to hiring faculty and staff.',
+		'link':null,
+		'type':'equal',
+		'history':false,
+	},
+
+	{
+		'date':'1944',
+		'title':'GI Bill',
+		'content':'Servicemen Readjustment Act (commonly referred to as the GI Bill) passes, making college more affordable and accessible to veterans.',
+		'link':'https://en.wikipedia.org/wiki/G.I._Bill',
+		'type':'va',
+		'history':true,
+	},
+
+	{
+		'date':'1913',
+		'title':'First WIT Graduate of Color',
+		'content':'Vincent B. Robinson earns a Machine Work Certificate and becomes first person of color to graduate from Wentworth.',
+		'link':null,
+		'type':'race',
+		'history':false,
+	},
+];
---
+++ js/types.js
@@ -0,0 +1,9 @@
+var types = {
+	"race": {"pic":"race.png", "bg":"#ffffff"},
+	"va": {"pic":"va.png", "bg":"#ffffff"},
+	"women": {"pic":"women.png", "bg":"#ffffff"},
+	"equal": {"pic":"equal.png", "bg":"#ffffff"},
+	"international": {"pic":"international.png", "bg":"#ffffff"},
+	"disability": {"pic":"disability.png", "bg":"#ffffff"},
+	"religion": {"pic":"religion.png", "bg":"#ffffff"}
+};
